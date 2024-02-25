ce616e325a6e9b1ff629f2bfff5695f85d4df55c
mushbari <154473061+mushbari@users.noreply.github.com>
2024-02-25 21:49:37 +0300

    water2024.py

--- water2024.py
+++ water2024.py
@@ -8,7 +8,7 @@ data_path = r"C:\\Users\\mmnm2\\Desktop\\PROJECT 45 END\\water.csv"
 df = pd.read_csv(data_path)
 
 # Load the trained model
-model_path = r"C:\\Users\\mmnm2\\Downloads\\RandomForestClassifier_model1.sav"
+model_path = r"C:\Users\mmnm2\Desktop\PROJECT 45 END\RandomForestClassifier_model1.sav"
 def load_model():
     try:
         if os.path.exists(model_path):
